8727d3e8a67aa7352470f62d9fd37f4c5d71dffd
Mustafa Ünal <mustafaunaldev@gmail.com>
2023-03-08 11:03:10 +0300

    first commit

---
+++ PdfMaker/Program.cs
@@ -0,0 +1,40 @@
+﻿using WkHtmlToPdfDotNet;
+
+var htmlFilePath = @"template.html";
+
+Dictionary<string, object> values = new() {
+    { "{{INVOICE_NUMBER}}", 15623 },
+    { "{{CREATE_DATE}}", DateOnly.FromDateTime(DateTime.Now) },
+    { "{{DUE_DATE}}", DateOnly.FromDateTime(DateTime.Now.AddYears(1)) },
+    { "{{ADDRESS}}", "Sefaköy<br/>Küçükçekmece<br/>İSTANBUL" },
+    { "{{CORP_NAME}}", "RSU Bilişim" },
+    { "{{FULL_NAME}}", "Mustafa Ünal" },
+    { "{{EMAIL}}", "mustafa.unal@rsu.com.tr" },
+};
+var htmlContent = File.ReadAllText(htmlFilePath).ReplaceBulk(values);
+
+var converter = new SynchronizedConverter(new PdfTools());
+var doc = new HtmlToPdfDocument()
+{
+    GlobalSettings = {
+        ColorMode = ColorMode.Color,
+        Orientation = Orientation.Portrait,
+        Margins = new MarginSettings(0,0,0,0),
+        PaperSize = PaperKind.A4,
+        Out = "out.pdf",
+    },
+    Objects = {
+        new ObjectSettings() {
+            //PagesCount = true,
+            HtmlContent = htmlContent,
+            WebSettings = { DefaultEncoding = "utf-8" },
+            //HeaderSettings = { FontSize = 9, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 }
+        }
+    }
+};
+converter.Convert(doc);
+
+// Show the PDF
+//System.Diagnostics.Process.Start(@"c:\out.pdf");
+
+//Console.ReadKey();
---
+++ PdfMaker/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿public static class StringExtensions
+{
+
+    public static string ReplaceBulk(this string s, Dictionary<string, object> values)
+    {
+        foreach (var item in values)
+        {
+            s = s.Replace(item.Key, item.Value.ToString());
+        }
+        return s;
+    }
+}
+
